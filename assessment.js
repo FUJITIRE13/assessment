8d3bbd77d71b9eaac2bec7f2154a0f521349fc4a
FUJITIRE13 <puripuriproject13@gmail.com>
2026-06-03 14:07:22 +0900

    Update assessment.js
    Added tweet function

--- assessment.js
+++ assessment.js
@@ -28,8 +28,18 @@ assessmentButton.addEventListener(
     paragraph.innerText = result;
     resultDivision.appendChild(paragraph);
 
-    // TODO ツイートエリアの作成
+    // ツイートエリアの作成
     tweetDivision.innerText = '';
+    const anchor = document.createElement('a');
+    const hrefValue =
+      'https://twitter.com/intent/tweet?button_hashtag=あなたのいいところ&ref_src=twsrc%5Etfw';
+
+    anchor.setAttribute('href', hrefValue);
+    anchor.setAttribute('class', 'twitter-hashtag-button');
+    anchor.setAttribute('data-text', '診断結果の文章');
+    anchor.innerText = 'Tweet #あなたのいいところ';
+
+    tweetDivision.appendChild(anchor);
   }
 );
 
